d32de0d2616a98ab658e84dd176723f29bc0b8f2
Gustavo Souza Gonçalves <gustavosouzagoncalves@outlook.com>
2019-09-23 00:50:48 -0300

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
 import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -14,8 +12,6 @@ import { WizardFinishedComponent } from './wizard-finished/wizard-finished.compo
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    AboutComponent,
     PersonalDetailsComponent,
     ContactDetailsComponent,
     SkillsComponent,
